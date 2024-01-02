d6bfc6117f7f5b8a9099134c93ba6712b7fe7786
ZhuHongK <1711486550@qq.com>
2024-01-02 22:10:10 +0800

    Logger

---
+++ Logger.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+  
+#include "noncopyable.h"
+
+// LOG_INFO("%s %d", arg1, arg2)
+#define LOG_INFO(logmsgFormat, ...) \
+    do \
+    { \
+        Logger &logger = Logger::instance(); \
+        logger.setLogLevel(INFO); \
+        char buf[1024] = {0}; \
+        snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
+        logger.log(buf); \
+    } while(0)
+
+// 定义日志的级别 INFO ERROR FATAL DEBUG
+enum
+{
+    INFO, // 普通信息
+    ERROR, // 错误信息
+    FATAL, // core信息 崩溃了
+    DEBUG, // 调试信息
+};
+
+// 输出一个日志类
+class Logger : noncopyable
+{
+public:
+    // 获取日志唯一的实例对象
+    static Logger& instance();
+    // 设置日志级别
+    void setLogLevel(int level);
+    // 写日志
+    void log(std::string msg);    
+private:
+    int logLevel_;
+    Logger() {}
+};

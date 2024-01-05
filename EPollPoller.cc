22389a470da05cab55f5e73920cda7a4a40f893f
zhk <1711486550@qq.com>
2024-01-05 10:37:17 +0800

    EPollPoller

---
+++ EPollPoller.cc
@@ -0,0 +1,24 @@
+#include "EPollPoller.h"
+#include "Logger.h"
+
+#include <error.h>
+
+const int kNew = -1;
+const int kAddd = 1;
+const int kDeleted = 2;
+
+EPollPoller::EPollPoller(EventLoop *loop)
+    : Poller(loop)
+    , epollfd_(::epoll_create1(EPOLL_CLOEXEC))
+    , events_(kInitEventListSize) // vector<epoll_event>
+{
+    if (epollfd_ < 0)
+    {
+        LOG_FATAL("epoll_create error:%d \n", errno);
+    }
+}
+
+EPollPoller::~EPollPoller()
+{
+
+}
